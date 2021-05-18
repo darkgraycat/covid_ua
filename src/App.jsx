75c739d6638553f9e4e3325e1fd2a9aef90b1b62
darkgraycat <jburyanov@gmail.com>
2021-05-18 16:33:08 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react'
-import './App.scss'
-import cat from '../assets/img/dgcat_ava.png'
 
 const App = () => {
   return (
     <div className='App'>
-      <h1>Minimal React App</h1>
-      <img src={cat} alt='cat' />
-      <h4>Includes:</h4>
-      <ul>
-        <li>sass-loader</li>
-        <li>file-loader</li>
-      </ul>
-      <h4>Enjoy!</h4>
-      <p>
-        Please give me feedback <a href="https://t.me/darkgraycat">Telegram</a>
-      </p>
-      <p>
-        If you have a problem: <a href="https://github.com/darkgraycat/jarb/issues">Issues</a>
-      </p>
+      <h1>Hellow</h1>
     </div>
   )
 }
