e86d789710d78a92cdef9d39041aafdaa169544e
darkgraycat <jburyanov@gmail.com>
2021-05-20 14:02:46 +0300

    mvp done

--- src/components/RegionCard.jsx
+++ src/components/RegionCard.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import names from '../regionNames.js'
+import Scale from './Scale.jsx'
 
 const CENTER = [49.0139, 32.2858]
 const SCALEX = 100
 const SCALEY = 150
+
+const STRINGS = {
+  'Active': 'Активно',
+  'Confirmed': 'Підтверджено',
+  'Recovered': 'Виліковано',
+  'Deaths': 'Померло'
+}
+
 const RegionCard = ({ region }) => {
 
   const { Province_State, Lat, Long_ } = region
+  const { Active, Confirmed, Recovered, Deaths } = region
 
   return (
     <div className='RegionCard' style={{
@@ -17,6 +27,19 @@ const RegionCard = ({ region }) => {
       <div className="RegionCard__title">
         {names[Province_State]}
       </div>
+      <div className="RegionCard__desc">
+        <p>{STRINGS.Active}: <span>{Active}</span></p>
+        <p>{STRINGS.Confirmed}: <span>{Confirmed}</span></p>
+        <p>{STRINGS.Recovered}: <span>{Recovered}</span></p>
+        <p>{STRINGS.Deaths}: <span>{Deaths}</span></p>
+      </div>
+
+      <Scale
+        active={Active}
+        confirmed={Confirmed}
+        recovered={Recovered}
+        deaths={Deaths}
+      />
     </div>
   )
 }
--- src/components/Scale.jsx
+++ src/components/Scale.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const Scale = ({ a, b, c }) => {
+const FACTOR = 0.002
+
+const Scale = ({ active, confirmed, recovered, deaths }) => {
+  const sizes = [
+    (confirmed * FACTOR) + 'px',
+    (recovered * FACTOR) + 'px',
+    (active * FACTOR) + 'px',
+    (deaths * FACTOR) + 'px'
+  ]
+  const colors = ['#8882', '#8f82', '#f888', '#000f']
+
   return (
-    <div></div>
+    <div className="Scale">
+      <div className="Scale__item" style={{ width: sizes[0], height: sizes[0], backgroundColor: colors[0] }}></div>
+      <div className="Scale__item" style={{ width: sizes[1], height: sizes[1], backgroundColor: colors[1] }}></div>
+      <div className="Scale__item" style={{ width: sizes[2], height: sizes[2], backgroundColor: colors[2] }}></div>
+      <div className="Scale__item" style={{ width: sizes[3], height: sizes[3], backgroundColor: colors[3] }}></div>
+    </div>
   )
 }
 
