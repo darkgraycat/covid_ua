845ecc9d86dac42f4e8159ec9da6691b338023fd
darkgraycat <jburyanov@gmail.com>
2021-06-01 18:31:41 +0300

    split chart to 3 parts

--- src/components/RegionChart.jsx
+++ src/components/RegionChart.jsx
@@ -8,22 +8,25 @@ const RegionChart = ({ history }) => {
     return `rgb(${~~(Math.random() * 255)},${~~(Math.random() * 255)},${~~(Math.random() * 255)})`
   }
 
-  const createData = () => {
+  const createData = (from, to) => {
     const dayN = new Date().getDay()
     const labels = strings.days.map((day, i, arr) => arr[(i - 1 + dayN) % 7])
     const datasets = []
     history.map((dayData, i) => {
       dayData.map((regionData, j) => {
-        if (!datasets[j]) {
-          const color = getRandomColor()
-          datasets[j] = {
-            label: strings.regions[regionData.Province_State],
-            backgroundColor: color,
-            borderColor: color,
-            data: [],
+        if (j >= from && j < to) {
+          const index = j - from
+          if (!datasets[index]) {
+            const color = getRandomColor()
+            datasets[index] = {
+              label: strings.regions[regionData.Province_State],
+              backgroundColor: color,
+              borderColor: color,
+              data: [],
+            }
           }
+          datasets[index].data[i] = regionData.Active
         }
-        datasets[j].data[i] = regionData.Active
       })
     })
     return {
@@ -32,15 +35,14 @@ const RegionChart = ({ history }) => {
     }
   }
 
-  const createChart = (data) => {
+  const createChart = (data, element) => {
     Chart.register(...registerables)
     new Chart(
-      document.getElementById('chart-canvas'),
+      element,
       {
         type: 'line',
         data,
         options: {
-          maintainAspectRatio: false,
           responsive: true
         }
       }
@@ -48,12 +50,16 @@ const RegionChart = ({ history }) => {
   }
 
   useEffect(() => {
-    createChart(createData())
+    createChart(createData(0, 9), document.getElementById('chart-canvas-1'))
+    createChart(createData(9, 18), document.getElementById('chart-canvas-2'))
+    createChart(createData(18, 27), document.getElementById('chart-canvas-3'))
   }, [])
 
   return (
     <div className="RegionChart">
-      <canvas id='chart-canvas'></canvas>
+      <canvas id='chart-canvas-1'></canvas>
+      <canvas id='chart-canvas-2'></canvas>
+      <canvas id='chart-canvas-3'></canvas>
     </div>
   )
 }
